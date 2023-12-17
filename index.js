080ce97de2d2561e281378f307411c7fae62af3e
Anselmo <ans.alv.46@gmail.com>
2023-12-17 15:17:00 -0600

    added mongodb

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const hostname = '0.0.0.0';
 const port = 3000;
 
+const mongoDB = 'mongodb://root:wMwonzj=yGq8@35.175.199.244:27017';
+
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
