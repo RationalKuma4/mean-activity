106d83572651bf16ddc9b89112035eda1e2866da
Anselmo <ans.alv.46@gmail.com>
2023-12-17 16:35:29 -0600

    open again

--- app.js
+++ app.js
@@ -6,28 +6,25 @@ const hostname = '0.0.0.0';
 const port = 3000;
 
 // MongoDB connection string
-// const mongoDB = 'mongodb://root:wMwonzj=yGq8@35.175.199.244:27017';
-// mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoDB = 'mongodb://root:wMwonzj=yGq8@35.175.199.244:27017';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Get the default connection
-// const db = mongoose.connection;
-// const localDb = db.useDb('local');
+const db = mongoose.connection;
+const localDb = db.useDb('local');
 
 // Bind connection to error event (to get notification of connection errors)
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// const StartupLog = localDb.model('StartupLog', new mongoose.Schema({}, { strict: false }), 'startup_log');
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+const StartupLog = localDb.model('StartupLog', new mongoose.Schema({}, { strict: false }), 'startup_log');
 
 const server = http.createServer(async (req, res) => {
     try {
         // Query the database
-        //const logs = await StartupLog.find({});
-        // res.statusCode = 200;
-        // res.setHeader('Content-Type', 'text/plain');
-        // Convert logs to string and append to response
-        //res.end('Actividad 2 - Jorge Anselmo Alvarado Balbuena\n' + JSON.stringify(logs, null, 2));
+        const logs = await StartupLog.find({});
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
-        res.end('Actividad 1 - Jorge Anselmo Alvarado Balbuena\n');
+        // Convert logs to string and append to response
+        res.end('Actividad 2 - Jorge Anselmo Alvarado Balbuena\n' + JSON.stringify(logs, null, 2));
     } catch (err) {
         console.error(err);
         res.statusCode = 500;
