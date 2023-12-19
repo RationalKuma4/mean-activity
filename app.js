d9d2737cbdfeda5b7e676fe52d358d5b601eaef3
Anselmo <ans.alv.46@gmail.com>
2023-12-18 19:16:50 -0600

    update

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const http = require('http');
 
 const hostname = '0.0.0.0';
+//const hostname = 'localhost';
 const port = 3000;
 
-const mongoDB = 'mongodb://root:wMwonzj=yGq8@35.175.199.244:27017';
+//const mongoDB = 'mongodb://root:wMwonzj=yGq8@35.175.199.244:27017';
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Actividad 2 - Jorge Anselmo Alvarado Balbuena\nMongo connection string es mongodb://root:wMwonzj=yGq8@35.175.199.244:27017');
+    res.end('Actividad 2 - Jorge Anselmo Alvarado Balbuena\n');
 });
 
 server.listen(port, hostname, () => {
