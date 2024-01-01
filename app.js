82fe81bb3cff82669d668802b233b93d42b249aa
Anselmo <ans.alv.46@gmail.com>
2024-01-01 15:27:15 -0600

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3001; // Puerto cambiado a 3001
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Herramientas DevOps - Actividad 2 - Jorge Alvarado\nmongodb connection string mongodb://root[user]:wMwonzj[password]=yGq8@35.175.19.2[mongodb server ip]:27017\n');
+    res.end('mongodb://root:wMwonzj=yGq8@35.175.19.245:27017\n');
 });
 
 server.listen(port, hostname, () => {
--- index.js
+++ index.js
@@ -1,56 +1,56 @@
-// const http = require('http');
+const http = require('http');
 
-// const hostname = '0.0.0.0';
-// const port = 3000;
+const hostname = '0.0.0.0';
+const port = 3000;
 
-// const mongoDB = 'mongodb://root:wMwonzj=yGq8@35.175.199.244:27017';
+const mongoDB = 'mongodb://root:wMwonzj=yGq8@35.175.199.244:27017';
 
-// const server = http.createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.end('Actividad 1 - Jorge Anselmo Alvarado Balbuena\n');
-// });
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Actividad 1 - Jorge Anselmo Alvarado Balbuena\n');
+});
 
-// server.listen(port, hostname, () => {
-//     console.log(`Server running at http://${hostname}:${port}/`);
-// });
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 
-// const http = require('http');
-// const mongoose = require('mongoose');
+const http = require('http');
+const mongoose = require('mongoose');
 
-// const hostname = '0.0.0.0';
-// //const hostname = 'localhost';
-// const port = 3000;
+const hostname = '0.0.0.0';
+//const hostname = 'localhost';
+const port = 3000;
 
-// // MongoDB connection string
-// const mongoDB = 'mongodb://root:wMwonzj=yGq8@35.175.199.244:27017';
-// mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+// MongoDB connection string
+const mongoDB = 'mongodb://root:wMwonzj=yGq8@35.175.199.244:27017';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// // Get the default connection
-// const db = mongoose.connection;
-// const localDb = db.useDb('local');
+// Get the default connection
+const db = mongoose.connection;
+const localDb = db.useDb('local');
 
-// // Bind connection to error event (to get notification of connection errors)
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// const StartupLog = localDb.model('StartupLog', new mongoose.Schema({}, { strict: false }), 'startup_log');
+// Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+const StartupLog = localDb.model('StartupLog', new mongoose.Schema({}, { strict: false }), 'startup_log');
 
-// const server = http.createServer(async (req, res) => {
-//     try {
-//         // Query the database
-//         const logs = await StartupLog.find({});
-//         res.statusCode = 200;
-//         res.setHeader('Content-Type', 'text/plain');
-//         // Convert logs to string and append to response
-//         res.end('Actividad 2 - Jorge Anselmo Alvarado Balbuena\n' + JSON.stringify(logs, null, 2));
-//     } catch (err) {
-//         console.error(err);
-//         res.statusCode = 500;
-//         res.setHeader('Content-Type', 'text/plain');
-//         res.end('An error occurred while retrieving data from MongoDB\n');
-//     }
-// });
+const server = http.createServer(async (req, res) => {
+    try {
+        // Query the database
+        const logs = await StartupLog.find({});
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        // Convert logs to string and append to response
+        res.end('Actividad 2 - Jorge Anselmo Alvarado Balbuena\n' + JSON.stringify(logs, null, 2));
+    } catch (err) {
+        console.error(err);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('An error occurred while retrieving data from MongoDB\n');
+    }
+});
 
-// server.listen(port, hostname, () => {
-//     console.log(`Server running at http://${hostname}:${port}/`);
-// });
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
